523f62a2beb05e392bd6e931b5d509c3513d5698
sklucke <sklucke@sean>
2023-12-13 17:08:45 -0600

    app.js

--- app.js
+++ app.js
@@ -3,3 +3,21 @@ const API_URL ="https://fsa-crud-2aa9294fe819.herokuapp.com/api/2309-FTB-MT-WEB-
 const state = {
     events: [],
 };
+
+// ******************* Selectors*************
+const partyPlanner = document.querySelector("#addEvent");
+const partyPlanner2 = document.querySelector("#events");
+
+async function getPlanner() {
+    try {
+      const response = await fetch(API_URL);
+      const json = await response.json();
+      console.log("json", json.data);
+      state.recipes = json.data;
+    } catch (error) {
+      console.error(error, "There was an error /GET recipes");
+    }
+  }
+
+  getPlanner()
+  
